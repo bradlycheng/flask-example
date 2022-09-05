fcfee1ecf3896d0b9a9cebdc5c9d8c2d68f62ef5
b brad <bradlycheng@gmail.com>
2022-09-05 02:29:55 -0400

    initial commit

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from views import views
+
+application = Flask(__name__)
+application.register_blueprint(views, url_prefix="/views")
+
+if __name__ == '__main__':
+    application.run( debug=True, port=5000)
+
+# @application.route('/')
+# def hello_world():
+#     return 'Sup. Scub'
